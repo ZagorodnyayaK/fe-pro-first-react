9bd9be265de65073cc4dcbd0434f8a2f52b3375b
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2024-02-07 11:22:31 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {Header} from "./components/Header/Header";
+import {BodySite} from "./components/BodySite/BodySite";
+
+function App() {
+    return (
+        <>
+          <Header/>
+          <BodySite/>
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/BodySite/BodySite.jsx
@@ -0,0 +1,65 @@
+import "./BodySite.css";
+
+const itemsInfo = [
+    {
+        name: 'Water and sunrise',
+        img: "http://placehold.it/300x300",
+        size: '2 332 KB',
+        category: 'nature',
+        dimensions_pic: '4528 x 3200',
+        id: 1
+    },
+    {
+        name: 'Water and tree',
+        img: "http://placehold.it/300x300",
+        size: '2 332 KB',
+        category: 'nature',
+        dimensions_pic: '4028 x 2264',
+        id: 2
+    },
+    {
+        name: 'Water and mountains',
+        img: "http://placehold.it/300x300",
+        size: '2 332 KB',
+        category: 'nature',
+        dimensions_pic: '4928 x 3294',
+        id: 3
+    },
+    {
+        name: 'Water and mountains',
+        img: "http://placehold.it/300x300",
+        size: '2 332 KB',
+        category: 'nature',
+        dimensions_pic: '4928 x 3294',
+        id: 3
+    }
+]
+
+export function BodySite() {
+    return (
+        <div className="container">
+            <div className="navigation">
+                <ul>
+                    <li>Flower</li>
+                    <li>Nature</li>
+                    <li>Cozy work</li>
+                </ul>
+            </div>
+            <div className="description">
+                {
+                    itemsInfo.map((item) => {
+                        return (
+                                <div className="item-description" key={item.id}>
+                                <h2>{item.name}</h2>
+                                    <img className="item-img" src={item.img} alt="img"/>
+                                <p>{item.size}</p>
+                                <p>{item.category}</p>
+                                <p>{item.dimensions_pic}</p>
+                                <button>Download</button>
+                            </div>)
+                    })
+                }
+            </div>
+        </div>
+    )
+}
